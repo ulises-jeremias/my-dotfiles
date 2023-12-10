450666cf9cd5d3ae19c9d74d64567d84acefeaf5
ulises-jeremias <ulisescf.24@gmail.com>
2023-12-10 14:18:51 -0300

    Updated i3 config

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -31,9 +31,6 @@ dots-scripts feh-blur --blur 10 -d &
 # open polybar
 "${XDG_CONFIG_HOME}"/polybar/launch &
 
-# i3 layouts
-[[ -z "$(pgrep i3-layouts)" ]] && whereis i3-layouts && i3-layouts &
-
 # Start nm-applet
 [[ -z "$(pgrep nm-applet)" ]] && nm-applet &
 
